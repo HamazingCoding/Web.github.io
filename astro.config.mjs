1d9cc007fb513965da3a1f44bd7d130743fbda32
SparksLabTech <sparkslabtech@gmail.com>
2024-08-05 12:07:22 +0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // 'https://<your-github-account>.github.io'
+  site: 'https://HamazingCoding-github.io'
 });
